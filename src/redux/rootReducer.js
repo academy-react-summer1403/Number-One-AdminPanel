474b3ae91edcd5788fae4f83dbae23647f8beeab
Molaei <molaie.web.developer@gmail.com>
2024-11-12 12:09:04 +0000

    add redux to store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,9 @@
 // ** Reducers Imports
 import layout from "./layout";
 import navbar from "./navbar";
+import UserInfoSlice from "../view/user/store/UserInfoSlice";
+import FilterSlice from "../view/user/store/FilterSlice";
 
-const rootReducer = { navbar, layout };
+const rootReducer = { navbar, layout, UserInfoSlice, FilterSlice };
 
 export default rootReducer;
