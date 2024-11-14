d0c53e763be91d7e432e413d6dcd1c5c20a84456
Molaei <molaie.web.developer@gmail.com>
2024-11-14 12:38:26 +0000

    add news store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,7 +3,16 @@ import layout from "./layout";
 import navbar from "./navbar";
 import UserInfoSlice from "../view/user/store/UserInfoSlice";
 import FilterSlice from "../view/user/store/FilterSlice";
+import NewsList from "../view/news/store/NewsList";
+import NewsDetail from "../view/news/store/NewsDetail";
 
-const rootReducer = { navbar, layout, UserInfoSlice, FilterSlice };
+const rootReducer = {
+  navbar,
+  layout,
+  UserInfoSlice,
+  FilterSlice,
+  NewsList,
+  NewsDetail,
+};
 
 export default rootReducer;
