eeacd9ce140e668905c5ea3a505fca53396764a5
sobhan369arab <sobhanarab30@gmail.com>
2024-11-24 10:18:03 +0330

    course component

---
+++ src/pages/Courses.js
@@ -0,0 +1,13 @@
+import Courses from "../@core/components/courses/list";
+// ** Styles
+import '@styles/react/apps/app-ecommerce.scss'
+
+const CoursePage = () => {
+  return (
+    <>
+      <Courses />
+    </>
+  );
+};
+
+export default CoursePage;
