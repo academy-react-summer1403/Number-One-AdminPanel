3a2c5e15ba615c806d08b3633597ed292fd696aa
Molaei <molaie.web.developer@gmail.com>
2024-11-26 17:49:15 +0330

    add new constants for news

---
+++ src/@core/constants/news-manage/Options.js
@@ -0,0 +1,44 @@
+import { Activity, Book, Slash } from "react-feather";
+import { handleIsActive, handleSortingCol } from "../../../view/news/store/NewsList";
+
+export const StatisticsOfNews = (data) => {
+  const newsSummaryData = [
+    {
+      title: "مجموع اخبار و مقالات",
+      color: "primary",
+      icon: Book,
+      renderStats: data?.active + data?.unActive,
+    },
+    {
+      title: "اخبار و مقالات فعال",
+      color: "success",
+      icon: Activity,
+      renderStats: data?.active,
+    },
+    {
+      title: "اخبار و مقالات غیر فعال",
+      color: "warning",
+      icon: Slash,
+      renderStats: data?.unActive,
+    },
+  ];
+
+  return newsSummaryData;
+};
+
+export const NewsSortOption = [
+  {
+    Options: [
+      { value: true, label: "فعال" },
+      { value: false, label: "غیر فعال" },
+    ],
+    setState: handleIsActive,
+  },
+  {
+    Options: [
+      { value: "rate", label: "محبوب ترین ها" },
+      { value: "price", label: "ارزان ترین" },
+    ],
+    setState: handleSortingCol,
+  },
+];
