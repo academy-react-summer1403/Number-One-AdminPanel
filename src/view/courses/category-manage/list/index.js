0be91bf6813ca29956a5a2f6f6ffa3c6e057c642
sobhan369arab <sobhanarab30@gmail.com>
2024-11-30 11:06:59 +0330

    handle list searchBar gor technologies

--- src/view/courses/category-manage/list/index.js
+++ src/view/courses/category-manage/list/index.js
@@ -4,15 +4,26 @@ import GeneralStatistics from "../../../../@core/components/generalStatistics";
 import { useQueryWithoutDependencies } from "../../../../utility/hooks/useCustomQuery";
 import { StatisticsOfCourseTechnologies } from "../../../../@core/constants/courses/StatisticsOfCourses";
 import { GetCourseTechnologies } from "../../../../@core/services/api/get-api";
+import ListHeader from "../../../../@core/components/products-list/ListHeader";
+import ListSearchbar from "../../../../@core/components/products-list/ListSearchbar";
+import { handleQuery, handleRowsOfPage } from "../store/TechnologiesList";
+import { useSelector } from "react-redux";
 
 const CourseTechnologiesWrapper = () => {
+  // redux Params
+  const { PageNumber, RowsOfPage, FilteredList, AllList, Query } = useSelector(
+    (state) => state.TechnologiesList
+  );
   // getting data from Api with use Query
   const {
     data: CourseTechData,
     isSuccess: successGetTach,
     refetch,
-  } = useQueryWithoutDependencies("GET_COURSE_TECHNOLOGIES", GetCourseTechnologies);
-  console.log(CourseTechData)
+  } = useQueryWithoutDependencies(
+    "GET_COURSE_TECHNOLOGIES",
+    GetCourseTechnologies
+  );
+  // console.log(CourseTechData);
 
   return (
     <Fragment>
@@ -24,7 +35,25 @@ const CourseTechnologiesWrapper = () => {
             resize="12"
           />
         </Col>
-        {/* TechnologiesList */}
+        <Col md={9} xs={12}>
+          <div>
+            <Row>
+              <Col className="pt-2">
+                <ListHeader
+                  rowsFunc={handleRowsOfPage}
+                  styleDisplay={"hidden"}
+                  colWidth={"6"}
+                />
+              </Col>
+              <Col>
+                <ListSearchbar
+                  QueryFunction={handleQuery}
+                  width={"mb-1 w-100"}
+                />
+              </Col>
+            </Row>
+          </div>
+        </Col>
       </Row>
     </Fragment>
   );
