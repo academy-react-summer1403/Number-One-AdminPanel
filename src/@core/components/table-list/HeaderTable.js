d9df46cddfcff7b8d3da5a898109cdf74b4487e8
Molaei <molaie.web.developer@gmail.com>
2024-12-04 02:04:59 +0330

    fix bug

--- src/@core/components/table-list/HeaderTable.js
+++ src/@core/components/table-list/HeaderTable.js
@@ -13,6 +13,8 @@ const HeaderTable = ({
   buttonText,
   setVariantState,
   isCreate = true,
+  isSearching = true,
+  handleSearch,
 }) => {
   const dispatch = useDispatch();
 
@@ -43,39 +45,32 @@ const HeaderTable = ({
           xl="6"
           className="d-flex align-items-sm-center justify-content-xl-end justify-content-start flex-xl-nowrap flex-wrap flex-sm-row flex-column pe-xl-1 p-0 mt-xl-0 mt-1"
         >
-          <div className="d-flex align-items-center gap-75 table-header-actions">
-            <Button
-              className="add-new-user"
-              color="primary"
-              onClick={() => {
-                toggleSidebar();
-                setScheduleDetails("test");
-                setVariantState("create");
-              }}
-            >
-              {buttonText}
-            </Button>
-          </div>
-          <div className="d-flex align-items-center mb-sm-0 mb-1 me-1">
-            <Input
-              style={{ width: "300px" }}
-              id="searchBox"
-              placeholder="جستجو ..."
-              onChange={(e) => {
-                e.preventDefault();
-                dispatch(handleSearch(searchBox.value));
-              }}
-            />
-          </div>
           {isCreate && (
-            <div className="d-flex align-items-center table-header-actions">
+            <div className="d-flex align-items-center gap-75 table-header-actions">
               <Button
                 className="add-new-user"
                 color="primary"
-                onClick={toggleSidebar}
+                onClick={() => {
+                  toggleSidebar();
+                  setScheduleDetails("test");
+                  setVariantState("create");
+                }}
               >
                 {buttonText}
               </Button>
+            </div>
+          )}
+          {isSearching && (
+            <div className="d-flex align-items-center mb-sm-0 mb-1 me-1">
+              <Input
+                style={{ width: "300px" }}
+                id="searchBox"
+                placeholder="جستجو ..."
+                onChange={(e) => {
+                  e.preventDefault();
+                  dispatch(handleSearch(searchBox.value));
+                }}
+              />
             </div>
           )}
         </Col>
