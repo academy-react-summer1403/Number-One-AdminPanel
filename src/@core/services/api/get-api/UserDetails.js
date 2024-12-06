2c0ab02b331da8992d65a9d0cd9b00ea53b0a5bb
sobhan369arab <sobhanarab30@gmail.com>
2024-12-06 18:05:32 +0330

    fixbug

--- src/@core/services/api/get-api/UserDetails.js
+++ src/@core/services/api/get-api/UserDetails.js
@@ -1,11 +1,13 @@
 import http from "../../interceptor";
 
 const UserDetails = async (id) => {
-  try {
-    const response = await http.get(`User/UserDetails/${id}`);
-    return response;
-  } catch {
-    return false;
+  if (id) {
+    try {
+      const response = await http.get(`User/UserDetails/${id}`);
+      return response;
+    } catch {
+      return false;
+    }
   }
 };
 
